e13484d18c5311c10dcf39cd19e4fdc9705e8073
withstars <15535822816@163.com>
2017-09-24 19:32:53 +0800

    [update]获取客户端信息和服务器信息

--- src/main/java/com/blog/web/AdminController.java
+++ src/main/java/com/blog/web/AdminController.java
@@ -1,9 +1,12 @@
 package com.blog.web;
 
+import com.blog.dao.CommentDao;
 import com.blog.domain.Admin;
 import com.blog.domain.AdminLoginLog;
 import com.blog.service.AdminLoginLogService;
 import com.blog.service.impl.AdminLoginLogServiceImpl;
+import com.blog.service.impl.ArticleServiceImpl;
+import com.blog.service.impl.CommentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +21,30 @@ import java.util.Date;
 public class AdminController {
     @Autowired
     public AdminLoginLogServiceImpl adminLoginLogService;
+    @Autowired
+    public ArticleServiceImpl articleService;
+    @Autowired
+    public CommentServiceImpl commentService;
     @RequestMapping("/main")
     public ModelAndView toMain(HttpServletRequest request){
         ModelAndView modelAndView=new ModelAndView("admin/main");
-        String hostIp=request.getRemoteAddr();    //获取远程主机地址，如：127.0.0.1
+        String clientIp=request.getRemoteAddr();    //获取客户端IP，如：127.0.0.1
+        String hostIp=request.getLocalAddr();
+        int hostPort=request.getLocalPort();
         Date date = new Date();
         SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm");//设置日期格式
         String dates = df.format(date);
         Admin admin=(Admin) request.getSession().getAttribute("admin");
         AdminLoginLog adminLoginLog=adminLoginLogService.selectRencent(admin.getId());
-        System.out.print(admin.getId());
+        int articleCount=articleService.selectCount();
+        int commentCount=commentService.countAllNum();
+        modelAndView.addObject("clientIp",clientIp);
         modelAndView.addObject("hostIp",hostIp);
+        modelAndView.addObject("hostPort",hostPort);
         modelAndView.addObject("date",dates);
         modelAndView.addObject("loginLog",adminLoginLog);
+        modelAndView.addObject("articleCount",articleCount);
+        modelAndView.addObject("commentCount",commentCount);
         return modelAndView;
     }
 
